e44f26ae9e22c8935b17ea40a6aacbd043f98c1c
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-02-18 12:40:17 +0530

    fixes

--- JS/ShowLink.js
+++ JS/ShowLink.js
@@ -40,7 +40,7 @@ document.getElementById("fetchCard").addEventListener("click", () => {
   let inviteCopied = `
 Hey! Here's Surprise for you....
     I've made a Small Birthday Gift for You....
-        Here's The Secret Code for you to Access it: ${sessionStorage.getItem("linkToShare")}
+        Here's The Secret Code for you to Access it: '${sessionStorage.getItem("linkToShare")}'
 
         1. Go to the Site and Paste your Secret Code
         2. Click on Receive Card to see the Surprise
