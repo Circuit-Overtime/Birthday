da234f62a1e56eca3afe925b4960781dda263579
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-03-03 01:19:30 +0530

    made permenant changes to link creation

--- JS/birthdayCard.js
+++ JS/birthdayCard.js
@@ -47,101 +47,129 @@ function typeWriterErrorHTML(idOfTextHolder, textToType, speed) {
 document.getElementById("createCard").addEventListener("click", () => {
   generateCard();
 })
-function generateCard()
-{
+async function generateCard() {
   const randomCode = generateRandomCode(10);
-  const ageInput =  document.getElementById("inputAge").value.trim();
-  const nameInput =  document.getElementById("inputName").value.trim();
+  const ageInput = document.getElementById("inputAge").value.trim();
+  const nameInput = document.getElementById("inputName").value.trim();
   const messageInput = document.getElementById("includedMessage").value.trim();
   const customURL = document.getElementById("customURL").value.trim().toLowerCase();
   const timeNow = Date.now();
-  if((ageInput != "") && (nameInput != ""))
-  {
-    if(customURL == "")
-    {
-      var DocumentFound = true;
-      
-        while (DocumentFound) {
-          db.collection("links").doc(randomCode).get().then((doc) => {
-            if(!(doc.exists))
-            {
-              db.collection("links").doc(randomCode).set({
-                age : ageInput,
-                name : nameInput,
-                encodeURL : randomCode.toLowerCase(),
-                message : messageInput,
-                timeStamp : timeNow
-      
-              }).then((doc) => {
-                document.querySelector(".loaderContainer").style.opacity = "1";
-                document.querySelector(".loaderContainer").style.zIndex = "100";
-                setTimeout(() => {
-      
-                  document.getElementById("inputAge").value = "";
-                  document.getElementById("inputName").value = "";
-                  document.getElementById("includedMessage").value = "";
-                  document.getElementById("customURL").value = "";
-                  document.querySelector(".loaderContainer").style.opacity = "0";
-                  document.querySelector(".loaderContainer").style.zIndex = "-1";
-                  sessionStorage.setItem("linkToShare", randomCode)
-                  location.replace("linkShow.html");
-                }, 1200);
-              })
-              DocumentFound = false;
-            }
-            else
-            {
-              DocumentFound = true;
-            }
-          });
-        }
+
+  if (ageInput !== "" && nameInput !== "") {
+    document.querySelector(".loaderContainer").style.opacity = "1";
+    document.querySelector(".loaderContainer").style.zIndex = "100";
+      if (customURL === "") {
+
+          let documentExists = false;
+          do {
+
+              documentExists = await doesItExist(randomCode);
+          } while (documentExists);
+          document.querySelector(".loaderContainer").style.opacity = "0";
+          document.querySelector(".loaderContainer").style.zIndex = "-1";
+
+          await generateDataBase(randomCode, ageInput, nameInput, messageInput, timeNow);
+
       }
-        else
-        {
-          db.collection("links").doc(customURL).set({
-            age : ageInput,
-            name : nameInput,
-            encodeURL : customURL,
-            message : messageInput,
-            timeStamp : timeNow
-  
-          }).then((doc) => {
-            document.querySelector(".loaderContainer").style.opacity = "1";
-            document.querySelector(".loaderContainer").style.zIndex = "100";
-            setTimeout(() => {
-  
-              document.getElementById("inputAge").value = "";
-              document.getElementById("inputName").value = "";
-              document.getElementById("includedMessage").value = "";
-              document.getElementById("customURL").value = "";
-              document.querySelector(".loaderContainer").style.opacity = "0";
-              document.querySelector(".loaderContainer").style.zIndex = "-1";
-              sessionStorage.setItem("linkToShare", customURL);
-              location.replace("linkShow.html");
-              
-            }, 1200);
-          })
-        }
-    }
 
-    else 
-    {
-      typeWriterErrorHTML("errorText", "Name and Age Please")
-    }
-  
-
+      else
+      {
+          if (await checkCustomUrl(customURL)) {
+            document.querySelector(".loaderContainer").style.opacity = "0";
+            document.querySelector(".loaderContainer").style.zIndex = "-1";
+              typeWriterErrorHTML("errorText", "Try a different custom URL!");
+              return;
+          } else {
+              await generateDataBaseFromURL(customURL, ageInput, nameInput, messageInput, timeNow);
+          }
+      }
+  } else {
+      typeWriterErrorHTML("errorText", "Name and Age Please");
   }
+}
 
-  document.getElementById("inputAge").addEventListener("input", function(event) {
-    const input = event.target.value;
-    const sanitizedInput = input.replace(/\D/g, ''); // Remove non-digit characters
-    event.target.value = sanitizedInput; // Update input value
+async function doesItExist(docCode) {
+  try {
+      const docSnapshot = await db.collection("links").doc(docCode).get();
+      return docSnapshot.exists;
+  } catch (error) {
+      console.error("Error fetching document: ", error);
+      return false;
+  }
+}
+
+function generateDataBaseFromURL(customURL, ageInput, nameInput, messageInput, timeNow) {
+  db.collection("links").doc(customURL).set({
+      age: ageInput,
+      name: nameInput,
+      encodeURL: customURL,
+      message: messageInput,
+      timeStamp: timeNow
+  }).then((doc) => {
+      document.querySelector(".loaderContainer").style.opacity = "1";
+      document.querySelector(".loaderContainer").style.zIndex = "100";
+      setTimeout(() => {
+          clearInputsAndDisplayLoader();
+          sessionStorage.setItem("linkToShare", customURL);
+          location.replace("linkShow.html");
+      }, 1200);
+  });
+}
+
+async function generateDataBase(randomCode, ageInput, nameInput, messageInput, timeNow) {
+  try {
+      await db.collection("links").doc(randomCode).set({
+          age: ageInput,
+          name: nameInput,
+          encodeURL: randomCode.toLowerCase(),
+          message: messageInput,
+          timeStamp: timeNow
+      });
+      document.querySelector(".loaderContainer").style.opacity = "1";
+      document.querySelector(".loaderContainer").style.zIndex = "100";
+      setTimeout(() => {
+          clearInputsAndDisplayLoader();
+          sessionStorage.setItem("linkToShare", randomCode);
+          location.replace("linkShow.html");
+      }, 1200);
+  } catch (error) {
+      console.error("Error generating database: ", error);
+  }
+}
+
+async function checkCustomUrl(url) {
+  try {
+      const docSnapshot = await db.collection("links").doc(url).get();
+      return docSnapshot.exists;
+  } catch (error) {
+      console.error("Error fetching document: ", error);
+      return false;
+  }
+}
+
+function clearInputsAndDisplayLoader() {
+  document.getElementById("inputAge").value = "";
+  document.getElementById("inputName").value = "";
+  document.getElementById("includedMessage").value = "";
+  document.getElementById("customURL").value = "";
+  document.querySelector(".loaderContainer").style.opacity = "0";
+  document.querySelector(".loaderContainer").style.zIndex = "-1";
+}
+
+document.getElementById("inputAge").addEventListener("input", function(event) {
+  const input = event.target.value;
+  const sanitizedInput = input.replace(/\D/g, ''); // Remove non-digit characters
+  event.target.value = sanitizedInput; // Update input value
 });
 
 document.getElementById("redirectIcon").addEventListener("click", () => {
-  location.replace("linkRedirecrt.html");
-})
+  location.replace("linkRedirect.html");
+});
 
 document.querySelector(".birthdayCap").addEventListener("load", () => {
   document.querySelector(".birthdayCapOpt").style.opacity = "0";
-})
+});
+
+// Usage:
+
+
